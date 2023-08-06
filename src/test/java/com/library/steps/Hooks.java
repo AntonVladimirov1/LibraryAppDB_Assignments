0b66a2d80560c699aaf73bf73495713d1bb179f4
Antonio_Git <l.a.v7232@gmail.com>
2023-08-06 18:06:54 -0500

    current changes

--- src/test/java/com/library/steps/Hooks.java
+++ src/test/java/com/library/steps/Hooks.java
@@ -1,6 +1,7 @@
 package com.library.steps;
 
 import com.library.utility.ConfigurationReader;
+import com.library.utility.DB_Util;
 import com.library.utility.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -12,17 +13,13 @@ import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
-    @Before
+    @Before("@ui")
     public void setUp(){
         System.out.println("this is coming from BEFORE");
-        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        Driver.getDriver().manage().window().maximize();
         Driver.getDriver().get(ConfigurationReader.getProperty("library_url"));
-
-
     }
 
-    @After
+    @After("@ui")
     public void tearDown(Scenario scenario){
         System.out.println("this is coming from AFTER");
 
@@ -30,11 +27,19 @@ public class Hooks {
             final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
             scenario.attach(screenshot,"image/png","screenshot");
         }
-
         Driver.closeDriver();
-
     }
 
+    @Before("@db")
+    public void setupDB(){
+        System.out.println("connecting to DB...");
+        DB_Util.createConnection();
+    }
+    @After("@db")
+    public void closeDB(){
+        System.out.println("disconnecting DB...");
+        DB_Util.destroy();
+    }
 
 
 
---
+++ src/test/java/com/library/steps/UserStepDefs.java
@@ -0,0 +1,59 @@
+package com.library.steps;
+
+import com.library.utility.DB_Util;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.List;
+
+public class UserStepDefs {
+
+    String actualUsersCount;
+    String expectedUserCount;
+    List<String> actualColumnNames;
+
+    @Given("Establish the database connection")
+    public void establish_the_database_connection() {
+        System.out.println("Connection to DB - in Hooks class");
+    }
+
+    @When("Execute query to get all IDs from users")
+    public void execute_query_to_get_all_i_ds_from_users() {
+
+        String query = "select count(id) from users";
+        DB_Util.runQuery(query);
+
+        actualUsersCount = DB_Util.getFirstRowFirstColumn();
+        System.out.println(actualUsersCount);
+    }
+
+    @Then("verify all users has unique ID")
+    public void verify_all_users_has_unique_id() {
+
+        String query = "select count(distinct id) from users";
+        DB_Util.runQuery(query);
+
+        expectedUserCount = DB_Util.getFirstRowFirstColumn();
+        System.out.println(expectedUserCount);
+
+        Assert.assertEquals(expectedUserCount,actualUsersCount);
+    }
+    @When("Execute query to get all columns")
+    public void executeQueryToGetAllColumns() {
+
+        String query = "select * from users";
+        DB_Util.runQuery(query);
+        actualColumnNames = DB_Util.getAllColumnNamesAsList();
+    }
+
+    @Then("verify the below columns are listed in result")
+    public void verifyTheBelowColumnsAreListedInResult(List<String> expectedColumnNames) {
+
+        Assert.assertEquals(expectedColumnNames,actualColumnNames);
+        System.out.println(expectedColumnNames);
+        System.out.println(actualColumnNames);
+    }
+
+}
